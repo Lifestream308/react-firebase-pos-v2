c5f6bb4ad4e208e5d3eee9deb049e0e5bbc20f0c
Christian Arroyo <ChristianArroyo308@yahoo.com>
2022-12-16 17:18:51 -0800

    smallFix

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -7,7 +7,7 @@ export default function ItemComponent({ item, findTotal }) {
         <span>$</span>
         <span className='price'>{item.Price}</span>
         <br></br>
-        <input type='number' className='amount' onChange={findTotal} />
+        <input type='number' className='amount' onChange={findTotal} defaultValue='0' />
     </div>
   )
 }
