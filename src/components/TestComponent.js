85a52bfd2a3cb0a58fd90d0608d7bcc65b0f2447
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-02-01 18:04:32 -0800

    CSS progress on test component for side navigation

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -6,13 +6,13 @@ export default function TestComponent() {
     <div>
         <p>TestComponent</p>
 
-        <nav>
+        <nav className='Component__sideNav'>
             <ul>
                 <li><Link className='' to='/'>Home</Link></li>
                 <li><Link className='' to='/update'>Update</Link></li>
                 <li><Link className='' to='/history'>History</Link></li>
                 <li><Link className='' to='/about'>About</Link></li>
-                <li onClick={ () => alert('Logged Out test')}>Logout</li>
+                <li onClick={ () => alert('Logged Out test')}><Link className='' to='/'>Logout</Link></li>
             </ul>
         </nav>
 
